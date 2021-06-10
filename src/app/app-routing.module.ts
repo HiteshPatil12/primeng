ff5cca411d091bc62ee2e24b335e09114d5bff01
HiteshPatil12 <hiteshpatil394@gmail.com>
2021-06-10 17:19:56 +0530

    book component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { BookDataComponent } from './book-data/book-data.component';
+const routes: Routes = [
+  { path: 'books', component: BookDataComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
