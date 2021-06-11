d28f73ef222317091970403e38637d0c1d7df680
HiteshPatil12 <hiteshpatil394@gmail.com>
2021-06-11 17:35:34 +0530

    final push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookDataComponent } from './book-data/book-data.component';
+import { TableModule } from 'primeng/table';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { BookDataComponent } from './book-data/book-data.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/book-data/book-data.component.ts
+++ src/app/book-data/book-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService, Book } from '../book.service';
 
 @Component({
   selector: 'app-book-data',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDataComponent implements OnInit {
 
-  constructor() { }
+  books: Book[];
 
-  ngOnInit(): void {
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    this.bookService.getBooks().
+    then(books => this.books = books);
   }
 
 }
